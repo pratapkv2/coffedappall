c4dfd9e0550b968b66c3edd4477b456fc785be90
pratapkv2 <pratapkumarv4@gmail.com>
2016-01-20 16:23:12 +0530

    New coffeed app section 2

--- coffeedappall/settings.py
+++ coffeedappall/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'coffeedappall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
