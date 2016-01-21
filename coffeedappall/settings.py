affde44e20597846133e88b08ecb04388a8f8cff
pratapkv2 <pratapkumarv4@gmail.com>
2016-01-21 16:03:01 +0530

    Added images to aws

--- coffeedappall/settings.py
+++ coffeedappall/settings.py
@@ -76,16 +76,33 @@ WSGI_APPLICATION = 'coffeedappall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+ON_HEROKU = os.environ.get('ON_HEROKU')
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+
+
+
+
+if ON_HEROKU == True:
+
+    # Parse database configuration from $DATABASE_URL
+
+    import dj_database_url
+
+    DATABASES['default'] = dj_database_url.config()
+
+else: 
+
+    DATABASES = {
+
+        'default': {
+
+            'ENGINE': 'django.db.backends.sqlite3',
+
+            'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
+
+        }
+
+    }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -120,3 +137,10 @@ TEMPLATES_DIRS =  (
 STATICFILES_DIRS =  (
     os.path.join(MAIN_DIR,'static'),
     )
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_S3_FORCE_HTTP_URL = True
+AWS_QUERYSTRING_AUTH = False
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWSSecretKey')
+AWS_ACCESS_KEY_ID = os.environ.get('AWSAccessKeyId')
+AWS_STORAGE_BUCKET_NAME ='coffeedappall'
